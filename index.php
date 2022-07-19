a44d06a3d43020af94281d681612c8d785c321b2
FelixvL <digitaleservice@gmail.com>
2022-07-19 09:38:21 +0200

    next

--- index.php
+++ index.php
@@ -4,35 +4,25 @@ $mysqli = new mysqli("localhost","root","","databasevoormeubelwinkel");
 
 $sql = "SELECT * FROM Meubel";
 $result = $mysqli -> query($sql);
-
-while($rij = $result->fetch_assoc()){
-    echo $rij["naam"];
-}
-
-print("yes weet ik zeker");
-
 ?>
 
+    <style>
+        h3{
+            color:orange;
+        }    
+    </style>
+    <div>
+        <h1>Dit zijn onze meubels</h1>
+    </div>
 
-
-<style>
-    h3{
-        color:orange;
-    }    
-</style>
-<div>
-    <h1>Dit zijn onze meubels</h1>
-</div>
-<div>
-    <h3>Tafel</h3>
-    <img src="tafel.jpg" width="150px">
-
-</div>
-<div>
-    <h3>Stoel</h3>
-    <img src="stoel.jpg" width="150px">
-
-</div>
+<?php
+    while($rij = $result->fetch_assoc()){
+        echo "<div>
+            <h3>".$rij["naam"]."</h3>
+            <img src=".$rij["plaatje"]." width=150px>
+            </div>";
+    }
+?>
 <div>
     <hr>
 </div>
