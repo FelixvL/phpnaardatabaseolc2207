f9f9df1e724318830e31a595892413fa3bb12bcd
FelixvL <digitaleservice@gmail.com>
2022-07-19 08:59:37 +0200

    next

--- index.php
+++ index.php
@@ -3,3 +3,18 @@
 print("yes weet ik zeker");
 
 ?>
+
+
+
+
+
+<div>
+    <h1>Tafel</h1>
+    <img src="tafel.jpg" width="150px">
+
+</div>
+<div>
+    <h1>Stoel</h1>
+    <img src="stoel.jpg" width="150px">
+
+</div>
