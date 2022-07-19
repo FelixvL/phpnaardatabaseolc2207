920586265f258085d776fd9db4d1b80602b43d0f
FelixvL <digitaleservice@gmail.com>
2022-07-19 09:01:33 +0200

    next

--- index.php
+++ index.php
@@ -6,15 +6,24 @@ print("yes weet ik zeker");
 
 
 
-
-
+<style>
+    h3{
+        color:orange;
+    }    
+</style>
 <div>
-    <h1>Tafel</h1>
+    <h1>Dit zijn onze meubels</h1>
+</div>
+<div>
+    <h3>Tafel</h3>
     <img src="tafel.jpg" width="150px">
 
 </div>
 <div>
-    <h1>Stoel</h1>
+    <h3>Stoel</h3>
     <img src="stoel.jpg" width="150px">
 
+</div>
+<div>
+    <hr>
 </div>
